a214cf878344bf808e9951efdac5a4814afaef35
Lucas Lund <lucaslu@kth.se>
2024-08-04 22:41:26 +0200

    klkerkkre

--- Config.cs
+++ Config.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
-using DSharpPlus;
-using DSharpPlus.Entities;
+﻿using DSharpPlus.Entities;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
--- Games.cs
+++ Games.cs
@@ -25,6 +25,20 @@ public class Games
                // Define paths for the game folder and the current game
                string path = Path.Combine(Directory.GetCurrentDirectory(), "games");
                string filepath = Path.Combine(path,"currentGame.json");
+               
+               // Convert default_nations.yml to json
+               FileStream stream = File.OpenRead("nations.yml");
+
+               // Converts the FileStream into a YAML object (Young Adult Male Losers)
+               IDeserializer deserializer = new DeserializerBuilder().WithNamingConvention(HyphenatedNamingConvention.Instance).Build();
+               config = deserializer.Deserialize<Config>(new StreamReader(stream));
+               
+               // Create new json file
+               File.WriteAllText(filepath, "{}");
+          }
+          else
+          {
+               // tell user old game is still active.
           }
      }
 }
