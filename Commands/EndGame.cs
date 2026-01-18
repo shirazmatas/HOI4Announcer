b8583afe0a604a0d59c16d6f6dcdb34271cba432
Lucas Lund <lucaslu@kth.se>
2026-01-18 22:06:58 +0100

    Avengers Assemble

--- Commands/EndGame.cs
+++ Commands/EndGame.cs
@@ -1,6 +1,19 @@
-﻿namespace HOI4Announcer.Commands;
+﻿using System.ComponentModel;
+using DSharpPlus.Commands;
+
+namespace HOI4Announcer.Commands;
 
 public class EndGame
 {
-    
+    [Command("endgame")]
+    [Description("End a game and declare a winning side")]
+    public async Task OnExecute(CommandContext context, 
+        [Parameter("winner")] [Description("Pick a faction'" +
+                                           "or nation, " +
+                                           "or a tie")] string winner) 
+        // TODO: Change so winner is either a faction, a nation, or "tied"
+    {
+        // Active game set to no. Change text message within channel to say "Game ended" and display Trophy and who is the winner
+        // Other context added later?
+    }
 }
