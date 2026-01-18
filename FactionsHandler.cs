6014197090cca2fbfa0fc5ee52c15fa9629d09b5
Lucas Lund <lucaslu@kth.se>
2026-01-18 23:30:59 +0100

    FactionHandler created

---
+++ FactionsHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+
+namespace HOI4Announcer;
+
+public static class FactionsHandler
+{
+    public class FactionsConfig
+    {
+        public class Faction
+        {
+            public string name { get; set; }
+            public List<Nation> nations { get; set; }
+        }
+
+        public class Nation
+        {
+            public string name { get; set; }
+        }
+
+        public List<Faction> factions { get; set; }
+    }
+
+    public static FactionsConfig config;
+
+    private static readonly JsonSerializerOptions serializerOptions = new() { WriteIndented = true };
+
+    public static void Load()
+    {
+        config = JsonSerializer.Deserialize<FactionsConfig>(File.ReadAllText($"{Directory.GetCurrentDirectory()}/factions.json"));
+    }
+
+    public static void Save()
+    {
+        File.WriteAllText($"{Directory.GetCurrentDirectory()}/factions.json", JsonSerializer.Serialize(config, serializerOptions));
+    }
+}
+
