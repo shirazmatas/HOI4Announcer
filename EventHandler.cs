7b27da11cb7bc2a7f085509eaec94f102d2da15b
Lucas Lund <lucaslu@kth.se>
2026-01-18 20:25:04 +0100

    Fixed EventHandler.cs after 2 years

--- EventHandler.cs
+++ EventHandler.cs
@@ -1,13 +1,34 @@
 ﻿using DSharpPlus;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 
 namespace HOI4Announcer;
 
-public class EventHandler
+public static class EventHandler
 {
-    public static Task OnReady(DiscordClient client, SessionReadyEventArgs e)
+    internal static bool hasLoggedGuilds = false;
+
+    public static Task OnReady(DiscordClient client, GuildDownloadCompletedEventArgs e)
     {
-        Logger.Log("Connected to Discord.", LogID.DISCORD);
+        Logger.Log("Connected to Discord.");
+        hasLoggedGuilds = true;
         return Task.CompletedTask;
+    }
+
+    public static async Task OnGuildAvailable(DiscordClient discordClient, GuildAvailableEventArgs e)
+    {
+        if (hasLoggedGuilds)
+        {
+            return;
+        }
+
+        Logger.Log("Found Discord server: " + e.Guild.Name + " (" + e.Guild.Id + ")");
+
+        IReadOnlyDictionary<ulong, DiscordRole> roles = e.Guild.Roles;
+
+        foreach ((ulong roleID, DiscordRole role) in roles)
+        {
+            Logger.Debug(role.Name.PadRight(40, '.') + roleID);
+        }
     }
 }
