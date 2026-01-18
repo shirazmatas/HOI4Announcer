9280391ceefd7fca608c1418cdbc10a0035b8f5f
Lucas Lund <lucaslu@kth.se>
2026-01-18 22:06:35 +0100

    Change Faction started

--- Commands/ChangeFaction.cs
+++ Commands/ChangeFaction.cs
@@ -1,6 +1,14 @@
-﻿namespace HOI4Announcer.Commands;
+﻿using System.ComponentModel;
+using DSharpPlus.Commands;
+
+namespace HOI4Announcer.Commands;
 
 public class ChangeFaction
 {
-    
+    [Command("addnation")]
+    [Description("Add a nation to the roster of playable nations")]
+    public async Task OnExecute(CommandContext context,
+        [Parameter("nation")][Description("The nation to add")] Nations nation,
+        [Parameter("faction")][Description("The faction of the nation")] Factions faction)
+    {
 }
