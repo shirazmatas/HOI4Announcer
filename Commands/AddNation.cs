4e792d404964c65d820331a0ea7d2e8a93765ec5
Lucas Lund <lucaslu@kth.se>
2026-01-18 23:27:37 +0100

    AddNation logic

--- Commands/AddNation.cs
+++ Commands/AddNation.cs
@@ -11,8 +11,9 @@ public class AddNation
     [Command("addnation")]
     [Description("Add a nation to the roster of playable nations")]
     public async Task OnExecute(CommandContext context,
-        [Parameter("nation")][Description("The nation to add")] Nations nation,
-        [Parameter("faction")][Description("The faction of the nation")] Factions faction)
+        [Parameter("nation")][Description("The nation to add")] NationID nation,
+        [Parameter("faction")][Description("The faction of the nation")] Factions faction,
+        [Parameter("maxplayers")] [Description("The maximum number of players allowed for this nation")] int maxPlayers = 1)
     {
 
         try
@@ -23,7 +24,8 @@ public class AddNation
             //}
             // Check if nation is listed in allowed nations (exist in HOI4)
             FileStream stream = File.OpenRead("nations.yml");
-            IDeserializer deserializer = new DeserializerBuilder().WithNamingConvention(HyphenatedNamingConvention.Instance).Build();
+            IDeserializer deserializer = new DeserializerBuilder()
+                .WithNamingConvention(HyphenatedNamingConvention.Instance).Build();
             var nationsData = deserializer.Deserialize<Dictionary<string, List<string>>>(new StreamReader(stream));
 
             bool nationExists = false;
@@ -40,10 +42,18 @@ public class AddNation
 
             if (!nationExists)
             {
+                // Check if game exists
+                if (Games.HasActiveGame())
+                {
+                    // Add also to currentGame.json
+                }
+
+                // Add to nations.yml
+                Games.AddNation(nation, faction);
                 await context.RespondAsync($"Nation {nationName} has been added to the roster.");
                 return;
             }
-            
+
         }
         catch (Exception)
         {
